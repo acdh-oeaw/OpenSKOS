726cf720eabd8e31e87ee66290b3c877f450a275
root <root@clarin.arz.oeaw.ac.at>
2015-11-30 12:11:37 +0100

    Piwik tracking code added

--- public/browser/index.php
+++ public/browser/index.php
@@ -213,6 +213,22 @@ list($list,$total, $facetFieldCounts, $facetQueryCounts) = getList($start,$numbe
       $("form#formulier input.matchTermsExact").click(function(){ $("form#formulier").submit(); });
     });          
   </script>
+  <!-- Piwik -->
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//clarin.oeaw.ac.at/piwik/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', 13]);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+<noscript><p><img src="//clarin.oeaw.ac.at/piwik/piwik.php?idsite=13" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+
 </head>
 <body>
 <div id="wrap">
